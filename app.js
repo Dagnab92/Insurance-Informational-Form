82572075fd8f3a74a30e888c80cb000c5a19f94d
Dagnab92 <82834045+Dagnab92@users.noreply.github.com>
2022-08-11 08:30:19 +0200

    test

---
+++ app.js
@@ -0,0 +1,5 @@
+// show fields
+function showInfo(recommmended_by_physician_or_other_medical_paractioner){
+var more_details1= document.getElementById("more_details1");
+more_details1.style.display = showInfo.checked ? "block" : "none";
+}
